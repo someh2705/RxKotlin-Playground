a8de1fe3e06f9ddc7650d75d17a6bcd885a4f8d2
someh <someh2705@gmail.com>
2021-10-23 23:41:00 +0900

    feat: Publish subject

--- src/main/kotlin/example/helloSubject/HelloSubject.kt
+++ src/main/kotlin/example/helloSubject/HelloSubject.kt
@@ -2,6 +2,19 @@ package example.helloSubject
 
 class HelloSubject {
     fun main() {
-
+        val observable = Observable.interval(100, TimeUnit.MILLISECONDS) 
+        val subject = PublishSubject.create<Long>() 
+        
+        observable.subscribe(subject) 
+        
+        runBlocking { delay(300) } 
+        
+        subject.subscribe { println("1st: $it") } 
+        
+        runBlocking { delay(300) } 
+        
+        subject.subscribe { println("2nd: $it") } 
+        
+        runBlocking { delay(300) }
     }
 }
